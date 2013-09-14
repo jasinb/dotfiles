54052a93575fd054c50949c23e1715dd92592e94
Jasin Bushnaief <jasin@iki.fi>
2013-09-14 19:17:45 +0300

    Added install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# bash, append into corresponding files
+
+for f in $(pwd)/bash/*
+do
+	echo "source $f" >> ~/.`basename $f`
+done
+
+# vim
+
+ln -s $(pwd)/vim ~/.vim
+
+for f in $(pwd)/vim/*
+do
+	ln -s $f ~/.`basename $f`
+done
+
